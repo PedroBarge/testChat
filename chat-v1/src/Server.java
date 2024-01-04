7fa4869dd20ea17a170cb05e610f1dd1b79ffec0
PedroBarge <pedro525tavares@gmail.com>
2024-01-04 11:00:27 +0000

    Added check logs

--- chat-v1/src/Server.java
+++ chat-v1/src/Server.java
@@ -29,13 +29,16 @@ public class Server {
         boolean on = true;
         while (on) {
             System.out.println("Menu");
-            System.out.println("1-Start\n2-Exist");
+            System.out.println("1-Start\n2-Check Logs\n0-Exist");
             String option = scanner.next();
             switch (option) {
                 case "1":
                     start();
                     break;
                 case "2":
+                    checkAllLogs();
+                    break;
+                case "0":
                     on = false;
                     break;
                 default:
@@ -43,4 +46,35 @@ public class Server {
             }
         }
     }
+
+    private void checkAllLogs() {
+        FileInputStream fileInputStream;
+        try {
+            fileInputStream = new FileInputStream("src/DataBases/logsClients.txt");
+
+            String temp = readFromInputStream(fileInputStream);
+            System.out.println(temp);
+
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    private static String readFromInputStream(InputStream inputStream)
+            throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
 }
