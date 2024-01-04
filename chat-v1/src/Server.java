0892809c1f390ffa12d56f41f1b19c95e0129dcb
PedroBarge <pedro525tavares@gmail.com>
2024-01-04 15:39:41 +0000

    add a way to see the logs, add a way to close the server when is 0 users online

--- chat-v1/src/Server.java
+++ chat-v1/src/Server.java
@@ -64,6 +64,7 @@ public class Server {
             fileInputStream.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
