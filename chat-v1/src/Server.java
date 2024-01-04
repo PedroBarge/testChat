1d7551da9a6052383b8ddf2ab509427b971dcc49
PedroBarge <pedro525tavares@gmail.com>
2024-01-04 13:21:12 +0000

    Updates
    Added a menu to the server
    Added a request to add a name to the client

--- chat-v1/src/Server.java
+++ chat-v1/src/Server.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Server {
     private ServerSocket serverSocket;
@@ -9,9 +10,33 @@ public class Server {
     public Server() {
     }
 
+    boolean isOn = true;
+
+    public void menu() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (isOn) {
+            System.out.println("Menu");
+            System.out.println("1- Start\n0- Exit");
+            String input = scanner.next();
+            switch (input) {
+                case "1":
+                    start();
+                    scanner.reset();
+                    break;
+                case "0":
+                    isOn=false;
+                    scanner.reset();
+                    break;
+                default:
+                    System.out.println("Miss click");
+            }
+            scanner.reset();
+        }
+    }
+
     public void start() throws IOException {
         int onlineClients = 0;
-        boolean isOn = true;
+
         serverSocket = new ServerSocket(8666);
         System.out.println("Success " + serverSocket);
         System.out.println("Waiting for clients...");
