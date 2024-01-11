fefca95778ac91128c512962a69e14cc377499de
PedroBarge <pedro525tavares@gmail.com>
2024-01-11 12:31:59 +0000

    tenho que fazer um handler

--- chat-v1/src/Server.java
+++ chat-v1/src/Server.java
@@ -8,31 +8,6 @@ public class Server {
     public Server() {
     }
 
-    public void start() {
-        try {
-            serverSocket = new ServerSocket(8666);
-            System.out.println("Success " + serverSocket.getInetAddress() + serverSocket.getLocalPort());
-
-            while (true) {
-                System.out.println("Waiting for clients...");
-                Socket clientSocket = serverSocket.accept();
-                ClientManager clientManager = new ClientManager(clientSocket);
-                clientManager.run();
-
-                if (clientManager.thereIsUser()) {
-                    clientSocket.close();
-                    closeServer();
-
-                    break;
-                }
-            }
-
-        } catch (IOException e) {
-            System.out.println(e);
-            e.printStackTrace();
-        }
-    }
-
 //    public void start() {
 //        try {
 //            serverSocket = new ServerSocket(8666);
@@ -41,28 +16,54 @@ public class Server {
 //            while (true) {
 //                System.out.println("Waiting for clients...");
 //                Socket clientSocket = serverSocket.accept();
+                    //Thread handleClient = new Thread(() -> handleClient(serverSocket))
+//                ClientManager clientManager = new ClientManager(clientSocket);
+//                clientManager.run();
 //
-//                // Cria uma nova thread para lidar com o cliente
-//                new Thread(() -> {
-//                    ClientManager clientManager = new ClientManager(clientSocket);
-//                    clientManager.run();
+//                if (clientManager.thereIsUser()) {
+//                    clientSocket.close();
+//                    closeServer();
 //
-//                    if (clientManager.thereIsUser()) {
-//                        try {
-//                            clientSocket.close();
-//                            closeServer();
-//                            clientManager.interrupt();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }).start();
+//                    break;
+//                }
 //            }
+//
 //        } catch (IOException e) {
 //            System.out.println(e);
 //            e.printStackTrace();
 //        }
 //    }
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(8666);
+            System.out.println("Success " + serverSocket.getInetAddress() + serverSocket.getLocalPort());
+
+            while (true) {
+                System.out.println("Waiting for clients...");
+                Socket clientSocket = serverSocket.accept();
+
+                // Cria uma nova thread para lidar com o cliente
+                new Thread(() -> {
+                    ClientManager clientManager = new ClientManager(clientSocket);
+                    clientManager.run();
+
+                    if (clientManager.thereIsUser()) {
+                        try {
+                            clientSocket.close();
+                            closeServer();
+                            clientManager.interrupt();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
 
 
     private void closeServer() {
