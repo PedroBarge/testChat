be0079b82c76ed7ae1de5aeef8e0c2c943fa98fb
PedroBarge <pedro525tavares@gmail.com>
2024-01-11 12:19:26 +0000

    add comment code

--- chat-v1/src/Server.java
+++ chat-v1/src/Server.java
@@ -8,9 +8,7 @@ public class Server {
     public Server() {
     }
 
-    boolean foda = true;
-
-    /*public void start() {
+    public void start() {
         try {
             serverSocket = new ServerSocket(8666);
             System.out.println("Success " + serverSocket.getInetAddress() + serverSocket.getLocalPort());
@@ -24,7 +22,7 @@ public class Server {
                 if (clientManager.thereIsUser()) {
                     clientSocket.close();
                     closeServer();
-                    clientManager.interrupt();
+
                     break;
                 }
             }
@@ -33,38 +31,39 @@ public class Server {
             System.out.println(e);
             e.printStackTrace();
         }
-    }*/
-
-    public void start() {
-        try {
-            serverSocket = new ServerSocket(8666);
-            System.out.println("Success " + serverSocket.getInetAddress() + serverSocket.getLocalPort());
-
-            while (true) {
-                System.out.println("Waiting for clients...");
-                Socket clientSocket = serverSocket.accept();
-
-                // Cria uma nova thread para lidar com o cliente
-                new Thread(() -> {
-                    ClientManager clientManager = new ClientManager(clientSocket);
-                    clientManager.run();
-
-                    if (clientManager.thereIsUser()) {
-                        try {
-                            clientSocket.close();
-                            closeServer();
-                            clientManager.interrupt();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-            }
-        } catch (IOException e) {
-            System.out.println(e);
-            e.printStackTrace();
-        }
     }
+
+//    public void start() {
+//        try {
+//            serverSocket = new ServerSocket(8666);
+//            System.out.println("Success " + serverSocket.getInetAddress() + serverSocket.getLocalPort());
+//
+//            while (true) {
+//                System.out.println("Waiting for clients...");
+//                Socket clientSocket = serverSocket.accept();
+//
+//                // Cria uma nova thread para lidar com o cliente
+//                new Thread(() -> {
+//                    ClientManager clientManager = new ClientManager(clientSocket);
+//                    clientManager.run();
+//
+//                    if (clientManager.thereIsUser()) {
+//                        try {
+//                            clientSocket.close();
+//                            closeServer();
+//                            clientManager.interrupt();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }).start();
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e);
+//            e.printStackTrace();
+//        }
+//    }
+
 
     private void closeServer() {
         try {
